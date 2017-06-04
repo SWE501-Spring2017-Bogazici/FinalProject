75c39e5edd68432105d28403bc887b09eaf0bf5a
Fatih Parlayan <fatihparlayan34@gmail.com>
2017-06-05 00:22:02 +0300

    Create Output.java

---
+++ Output.java
@@ -0,0 +1,13 @@
+/**
+ * Created by fparlayan on 6/4/2017.
+ */
+public class Output {
+
+    private String name = "Output  ";
+    private double quantum_value;
+
+    public Output(String name, double quantum_value) {
+        this.name = this.name+name;
+        this.quantum_value = quantum_value;
+    }
+}
